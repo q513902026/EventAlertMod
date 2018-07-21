d506375b21dcfc2a14ebd4f168d9404727cd4a89
HopeAsd <qfeizaijun@qq.com>
2018-07-22 02:11:43 +0800

    初次提交7.3.0版本 用于对比更改

---
+++ libs/UTIL/util.lua
@@ -0,0 +1,13 @@
+hooksecurefunc("CreateFrame", function (frameType, frameName)
+	-- This detects when CompactRaidFrame1 gets created
+	-- and then displays the secure status of the frame.
+	if (frameName and frameName:match("CompactRaidFrame")) then
+		print(frameName, "has just been created.");
+		local secure, addon = issecurevariable(_G, frameName);
+		if (not secure) then
+			print(frameName, "is tainted by", addon);
+		else
+			-- print(frameName, "is secure.");
+		end
+	end
+end);
